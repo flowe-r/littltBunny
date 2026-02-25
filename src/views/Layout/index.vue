c45ad594119160ff7a469db7e4427e35d3016d5f
hyunjin <2321870748@qq.com>
2026-02-25 15:36:39 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,15 @@
-<template>
-    <div>
-        我是首页
-    <!-- 二级路由出口 -->
-     <RouterView />
-    </div>
-</template>
+<script setup>
+
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+
+</script>
+
+<<template>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
+</template>>
+
