c278d696b253bcf805472e0fed881ea9d826454a
hyunjin <2321870748@qq.com>
2026-02-26 10:34:34 +0800

    Day2结束

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,10 +3,19 @@
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFixed from './components/LayoutFixed.vue'; 
+
+//触发获取列表导航
+import { useCategoryStore } from '@/stores/category';
+import {onMounted} from 'vue'
+
+const categoryState = useCategoryStore()
+onMounted(() => categoryState.getCategory())
 
 </script>
 
 <<template>
+    <LayoutFixed />
     <LayoutNav />
     <LayoutHeader />
     <RouterView />
